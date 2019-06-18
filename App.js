2965adcfb3773d20f470cf9f46bb35451673112a
soh335 <sugarbabe335@gmail.com>
2019-06-18 19:19:30 +0900

    update App.js

--- App.js
+++ App.js
@@ -1,49 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{marginTop: 30}}>
+        <Text style={styles.paragraph}>ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ</Text>
+        <Text style={styles.paragraph}>これでちょうど十文字これでちょうど十文字これでちょうど十文字これでちょうど十文字</Text>
+        <Text style={styles.paragraph}>これでちょうど十文字これでちょうど十文字</Text>
+        <Text style={styles.paragraph}>你好早上好你们好老师好你好早上好你们好老师好你好早上好你们好老师好</Text>
+        <Text style={styles.paragraph}>你好早上好你们好老师好</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  paragraph: {
   },
 });
+
